cbbeefd678daa152f8fb69790493ed7b6cd98290
João Azambuja <109508@aluno.uricer.edu.br>
2026-05-27 19:40:00 -0300

    Correção de Porta pro render

--- app.js
+++ app.js
@@ -79,3 +79,9 @@ app.put('/posts/:id', (req, res) => {
     post.conteudo = conteudo;
 
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
